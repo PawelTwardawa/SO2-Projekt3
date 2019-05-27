2f44c885e0ecfef681a7644a8b970c79e2ddb481
Isild <pimok96@gmail.com>
2019-05-28 00:54:44 +0200

    klasa statek

--- Ship.cpp
+++ Ship.cpp
@@ -1,1 +1,10 @@
 #include "Ship.hpp"
+
+Ship::Ship(int N, int C, int X, int Y, float S)
+{
+    nr = N;
+    capacity = C;
+    x = X;
+    y = Y;
+    speed = S;
+}
--- Ship.hpp
+++ Ship.hpp
@@ -1,6 +1,13 @@
-
+//Statek będzie posiadał: nr, prędkość pływania, pojemność, pozycja x i y
 
 class Ship
 {
+    int nr;
+    int capacity;
+    int x;
+    int y;
+    float speed;
+
+    Ship(int N, int C, int X, int Y, float S);
     
 }
