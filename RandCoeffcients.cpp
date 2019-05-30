70b1ba77b04163b3d94a2224145ea3662d4cd111
Isild <pimok96@gmail.com>
2019-05-30 23:59:31 +0200

    zmiana pogody gotowa

--- RandCoeffcients.cpp
+++ RandCoeffcients.cpp
@@ -12,6 +12,24 @@ void RandCoeffcients::Rand()
     {
         ocean->stormValue = rand() % 4;
         ocean->waveHight = rand() % 7 + 1;
+
+        int r = rand() % 4 + 1;
+        switch (r)
+        {
+        case 1:
+            ocean->windDirection = 'S';
+            break;
+        case 2:
+            ocean->windDirection = 'N';
+            break;
+        case 3:
+            ocean->windDirection = 'E';
+            break;
+        default:
+            ocean->windDirection = 'W';
+            break;
+        }   
+
         std::this_thread::sleep_for(std::chrono::milliseconds(4000 * timeOperation + (rand() % 100)));
     }    
 }
